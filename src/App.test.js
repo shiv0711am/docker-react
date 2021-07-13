b5d2159f56ae084684cb4bcf1231940f5a3103c5
Shivam Aggarwal <shiaggarwal@wundermancommerce.com>
2021-07-13 21:37:31 +0530

    inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, unmountComponentAtNode } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
